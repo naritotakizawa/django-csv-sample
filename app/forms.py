eb059356e21ea25d91d5f2f384b88ea0ecd14e24
naritotakizawa <toritoritorina@gmail.com>
2019-10-24 15:15:38 +0900

    拡張子チェックを、組み込みのバリデーターに

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,19 @@
 import csv
 import io
 from django import forms
+from django.core.validators import FileExtensionValidator
 from .models import Post
 
 
 class CSVUploadForm(forms.Form):
-    file = forms.FileField(label='CSVファイル', help_text='※拡張子csvのファイルをアップロードしてください。')
+    file = forms.FileField(
+        label='CSVファイル',
+        help_text='※拡張子csvのファイルをアップロードしてください。',
+        validators=[FileExtensionValidator(allowed_extensions=['csv'])]
+    )
 
     def clean_file(self):
         file = self.cleaned_data['file']
-
-        # ファイル名が.csvかどうかの確認
-        if not file.name.endswith('.csv'):
-            raise forms.ValidationError('拡張子がcsvのファイルをアップロードしてください')
 
         # csv.readerに渡すため、TextIOWrapperでテキストモードなファイルに変換
         csv_file = io.TextIOWrapper(file, encoding='utf-8')
@@ -25,7 +26,7 @@ class CSVUploadForm(forms.Form):
                 post = Post(pk=row[0], title=row[1])
                 self._instances.append(post)
         except UnicodeDecodeError:
-                raise forms.ValidationError('ファイルのエンコーディングや、正しいCSVファイルか確認ください。')
+            raise forms.ValidationError('ファイルのエンコーディングや、正しいCSVファイルか確認ください。')
 
         return file
 
